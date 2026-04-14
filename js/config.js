73c1d63aa167fbc6861d05aacda0d436d687b8f9
Zhaoyang7982 <Zhaoyang7982@users.noreply.github.com>
2026-04-14 00:12:22 +0000

    deploy: 0057c071664492103d10f5f3fb8a7a25deb269b1

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-window.__LOVE_RECORD_CONFIG__ = {"SUPABASE_URL": "", "SUPABASE_ANON_KEY": ""};
+window.__LOVE_RECORD_CONFIG__ = {"SUPABASE_URL": "https://bxxkukqfsttbieehwrsh.supabase.co", "SUPABASE_ANON_KEY": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4eGt1a3Fmc3R0YmllZWh3cnNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwOTg4OTksImV4cCI6MjA5MTY3NDg5OX0.CGNqVLrly7P52oHZZYf_psQtTa_J1rlhu9ljPQKHMiU"};
