93de7ee5f21049f582d66ec6ed0e9b8a02da5afa
Zhaoyang7982 <Zhaoyang7982@users.noreply.github.com>
2026-04-29 13:11:11 +0000

    deploy: ac1e8f2807ff530c6a59350e4b224a9c0011aafa

--- js/config.local.js
+++ js/config.local.js
@@ -3,5 +3,5 @@
   if (!g) { window.__LOVE_RECORD_CONFIG__ = {}; g = window.__LOVE_RECORD_CONFIG__; }
   g.SUPABASE_URL = "https://bxxkukqfsttbieehwrsh.supabase.co";
   g.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4eGt1a3Fmc3R0YmllZWh3cnNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwOTg4OTksImV4cCI6MjA5MTY3NDg5OX0.CGNqVLrly7P52oHZZYf_psQtTa_J1rlhu9ljPQKHMiU";
-  g.ACCOUNTS = {"female": {"username": "", "password": ""}, "male": {"username": "", "password": ""}};
+  g.ACCOUNTS = {"female": {"username": "Yuyu", "password": "0321"}, "male": {"username": "Qiqi", "password": "1112"}};
 })();
